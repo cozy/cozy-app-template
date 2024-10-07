84e0265ea659f7b14ec79c5fd0d8d33f2861cd97
cballevre <celestin.ballevre@protonmail.com>
2024-10-08 01:10:12 +0200

    feat: Add rsbuild config used inside cozy-web-data-proxy

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,54 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig, rspack } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginEjs } from 'rsbuild-plugin-ejs';
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginEjs(), pluginNodePolyfill(), pluginReact()],
+  output: {
+    cleanDistPath: true,
+    distPath: {
+      root: 'build'
+    }
+  },
+  html: {
+    template: './src/targets/browser/index.ejs',
+    title: 'Cozy Todos'
+  },
+  performance: {
+    chunkSplit: {
+      forceSplitting: {
+        cozy: /node_modules[\\/]cozy*/,
+      },
+    }
+  },
+  source: {
+    entry: {
+      index: './src/targets/browser/index.tsx'
+    }
+  },
+  tools: {
+    rspack: {
+      module: {
+        rules: [
+          { test: /\.webapp$/i, type: "json" }
+        ]
+      },
+      plugins: [
+        new rspack.CopyRspackPlugin({
+          patterns: [
+            {
+              from: 'manifest.webapp',
+            },
+            {
+              from: 'README.md'
+            },
+            {
+              from: 'LICENSE'
+            }
+          ],
+        })
+      ]
+    }
+  }
 });
